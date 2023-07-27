6a497634c54f9414c9afbc6388b13b3999b7ad97
Asaduzzaman Ali <theasad.dev@gmail.com>
2023-07-27 19:55:43 +0600

    Task 7 Completed

--- Task7.js
+++ Task7.js
@@ -1,29 +1,23 @@
-/* 
-Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
-*/
+function romanToInteger(roman) {
+    let num = charToInteger(roman.charAt(0));
+    let previous, current;
 
+    for (let i = 1; i < roman.length; i++) {
+        current = charToInteger(roman.charAt(i));
+        previous = charToInteger(roman.charAt(i - 1));
 
-
-function romanToInteger(str1) {
-    if (str1 == null) return -1;
-    let num = charToInteger(str1.charAt(0));
-    let pre, curr;
-
-    for (let i = 1; i < str1.length; i++) {
-        curr = charToInteger(str1.charAt(i));
-        pre = charToInteger(str1.charAt(i - 1));
-        if (curr <= pre) {
-            num += curr;
+        if (current <= previous) {
+            num += current;
         } else {
-            num = num - pre * 2 + curr;
+            num = num - previous * 2 + current;
         }
     }
 
     return num;
 }
 
-function charToInteger(c) {
-    switch (c) {
+function charToInteger(roman) {
+    switch (roman) {
         case 'I': return 1;
         case 'V': return 5;
         case 'X': return 10;
@@ -31,10 +25,10 @@ function charToInteger(c) {
         case 'C': return 100;
         case 'D': return 500;
         case 'M': return 1000;
-        default: return -1;
+        default: throw new Error('Invalid Roman numeral character: ' + roman);
     }
 }
 
-const roman = 'VII';
-const result = charToInteger(roman)
-console.log(result);
+const roman = 'XXI'
+const result = romanToInteger(roman)
+console.log(result)
